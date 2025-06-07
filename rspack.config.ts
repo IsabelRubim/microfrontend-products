e7b46aa8b6571ea10f715f251c58dbc1ad95cb4e
IsabelRubim <isabel_rubim@hotmail.com.br>
2025-06-06 23:37:10 -0300

    feat: add deploy config to netlify

--- rspack.config.ts
+++ rspack.config.ts
@@ -2,13 +2,16 @@ const { ModuleFederationPlugin } = require('@rspack/core').container;
 const { rspack } = require('@rspack/core');
 const path = require('path');
 
+const isProduction = process.env.NODE_ENV === 'production';
+const publicPath = isProduction ? '/' : 'http://localhost:3002/';
+
 /** @type {import('@rspack/cli').Configuration} */
 const config = {
   entry: './src/index.ts',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js',
-    publicPath: 'http://localhost:3001/',
+    publicPath,
   },
   devServer: {
     port: 3001,
