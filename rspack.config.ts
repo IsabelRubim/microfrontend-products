9c56d0e4428aa465a57e4cdcc30c3b6f3f1209ea
IsabelRubim <isabel_rubim@hotmail.com.br>
2025-06-07 00:12:04 -0300

    fix: adjust publicPath

--- rspack.config.ts
+++ rspack.config.ts
@@ -11,7 +11,7 @@ const config = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js',
-    publicPath,
+    publicPath: publicPath,
   },
   devServer: {
     port: 3001,
